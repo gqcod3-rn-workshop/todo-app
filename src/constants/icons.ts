94f024be3b1209106d41dba29c6937f883def9f0
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-10-20 23:55:42 -0500

    feat(constants): create centralized icons constant.

---
+++ src/constants/icons.ts
@@ -0,0 +1,9 @@
+import categoryEvent from "@/assets/icons/category-event.png";
+import categoryGoal from "@/assets/icons/category-goal.png";
+import categoryTask from "@/assets/icons/category-task.png";
+
+export const icons = {
+  categoryEvent,
+  categoryGoal,
+  categoryTask,
+};
