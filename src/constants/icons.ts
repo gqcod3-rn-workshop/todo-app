4ce42a567cfff7562a5e0dc4b40917f39b90af71
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-10-25 18:40:33 -0500

    chore: add calendar and clock icon assets

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -1,9 +1,13 @@
 import categoryEvent from "@/assets/icons/category-event.png";
 import categoryGoal from "@/assets/icons/category-goal.png";
 import categoryTask from "@/assets/icons/category-task.png";
+import iconCalendar from "@/assets/icons/icon-calendar.png";
+import iconClock from "@/assets/icons/icon-clock.png";
 
 export const icons = {
   categoryEvent,
   categoryGoal,
   categoryTask,
+  iconCalendar,
+  iconClock,
 };
