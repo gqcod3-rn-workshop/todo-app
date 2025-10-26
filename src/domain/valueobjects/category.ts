59f3f88c5dec18b0ec927f0130c35970f55d5444
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-10-26 15:59:41 -0500

    feat: add category value object.

---
+++ src/domain/valueobjects/category.ts
@@ -0,0 +1,19 @@
+/**
+ * Category Value Object
+ */
+enum Category {
+    /**
+     * Task Category
+     */
+    Task = 'task',
+    /**
+     * Event Category
+     */
+    Event = 'event',
+    /**
+     * Goal Category
+     */
+    Goal = 'goal'
+};
+
+export default Category;
