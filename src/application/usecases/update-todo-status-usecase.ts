f82646995e0111e9558da5954081434eea8f74a4
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-10-25 19:54:31 -0500

    feat: add method to repository interface and its use case.

---
+++ src/application/usecases/update-todo-status-usecase.ts
@@ -0,0 +1,26 @@
+import TodoRepository from "@/src/domain/repositories/todo-repository";
+
+/**
+ * Update Todo Status Use Case
+ * @summary
+ * This use case handles the updating of a todo task's completion status by interacting with the TodoRepository.
+ * 
+ * @method execute - Executes the use case to update the completion status of a todo task.
+ */
+class UpdateTodoStatusUseCase {
+
+    /**
+     * Creates an instance of UpdateTodoStatusUseCase.
+     * @param todoRepository  - An implementation of the TodoRepository interface to handle data operations.
+     */
+    constructor(private todoRepository: TodoRepository) { }
+
+    /**
+     * Executes the use case to update the completion status of a todo task.
+     * @param id - The unique identifier of the todo task to be updated.
+     * @param isCompleted - The new completion status of the todo task.
+     */
+    async execute(id: string, isCompleted: boolean): Promise<void> {
+        await this.todoRepository.updateTodoStatus(id, isCompleted);
+    }
+};
--- src/domain/repositories/todo-repository.ts
+++ src/domain/repositories/todo-repository.ts
@@ -25,6 +25,12 @@ interface TodoRepository {
      * @param todo - The TodoEntity object representing the new todo task to be created.
      */
     createTodo(todo: TodoEntity): Promise<void>;
+    /**
+     * Updates the completion status of a todo task.
+     * @param id - The unique identifier of the todo task to be updated.
+     * @param isCompleted - The new completion status of the todo task.
+     */
+    updateTodoStatus(id: string, isCompleted: boolean): Promise<void>;
 };
 
 export default TodoRepository;
